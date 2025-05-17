996d6fe73752fdac20c937c576fdb90d17096e45
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-05-17 17:19:00 -0400

    add dev helpers for keeping svg files in sync

--- src/app/components/SvgIcon.tsx
+++ src/app/components/SvgIcon.tsx
@@ -1,15 +1,24 @@
 import { icons } from '@/app/constants';
+import { dev } from '@/utils/env';
+import { validateSvg } from '@/utils/svg-validate';
 
 export function SvgIcon({ icon: key }: { icon: keyof typeof icons }) {
-  const { viewBox, dataPath } = icons[key];
+  const icon = icons[key];
+
+  (async () => {
+    if (dev) {
+      const svg = await import(`@/app/assets/${key}.svg?raw`);
+      validateSvg(key, svg.default, icon);
+    }
+  })();
 
   return (
     <svg
       fill="currentColor"
-      viewBox={viewBox}
+      viewBox={icon.viewBox}
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path d={dataPath}></path>
+      <path d={icon.dataPath}></path>
     </svg>
   );
 }
