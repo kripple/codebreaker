ddecfe43a3fa1ff0dae2ff94ca36beb017378daf
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-05-19 15:41:38 -0400

    update dependencies

--- src/types/fastify.d.ts
+++ src/types/fastify.d.ts
@@ -1,30 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import type { NodePgClient } from 'drizzle-orm/node-postgres';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import type { FastifyInstance as Instance } from 'fastify';
-
-// export interface FastifyInstance<
-//   HttpServer = Server,
-//   HttpRequest = IncomingMessage,
-//   HttpResponse = ServerResponse,
-// > {
-//   db: NodePgClient;
-// }
 
 declare module 'fastify' {
   export interface FastifyInstance extends Instance {
     db: ReturnType<typeof drizzle>;
   }
 }
-
-// import fastify from 'fastify';
-
-// declare module 'fastify' {
-//   export interface FastifyInstance<
-//     HttpServer = Server,
-//     HttpRequest = IncomingMessage,
-//     HttpResponse = ServerResponse,
-//   > {
-//     port: number;
-//   }
-// }
