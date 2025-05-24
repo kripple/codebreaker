1f1244c1e3abf24ca57030e41ec1ba60ef3370fe
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-05-24 16:50:02 -0400

    add attempts adapter

--- src/db/adapters/attempts.e2e.ts
+++ src/db/adapters/attempts.e2e.ts
@@ -1,39 +1,99 @@
-// import { expect, test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
-// import { createNewAttempt, getAttempts } from '@/db/adapters/attempts';
-// // import { createNewGame } from '@/db/adapters/daily_games';
-// import { createNewUser } from '@/db/adapters/users';
+import { createNewAdhocGame } from '@/db/adapters/adhoc_games';
+import { createNewAttempt, getAttempts } from '@/db/adapters/attempts';
+import { getOrCreateDailyGame } from '@/db/adapters/daily_games';
+import { createNewUser } from '@/db/adapters/users';
 
-// // start the database and server before running these tests
+test.describe('attempts', () => {
+  test.describe('daily game', () => {
+    test('get attempts', async () => {
+      const user = await createNewUser();
+      expect(user).toBeTruthy();
 
-// test.describe('attempts', () => {
-//   // test('get attempts', async () => {
-//   //   const user = await createNewUser();
-//   //   expect(user).toBeTruthy();
+      const game = await getOrCreateDailyGame(user);
+      expect(game).toBeTruthy();
 
-//   //   const game = await createNewGame(user);
-//   //   expect(game).toBeTruthy();
+      const attempts = await getAttempts(game);
+      expect(attempts).toBeTruthy();
+      expect(attempts).toHaveLength(0);
+    });
 
-//   //   const attempts = await getAttempts(game);
-//   //   expect(attempts).toBeTruthy();
-//   //   expect(attempts).toHaveLength(0);
-//   // });
+    test('make attempt', async () => {
+      const user = await createNewUser();
+      expect(user).toBeTruthy();
 
-//   // test('make attempt', async () => {
-//   //   const user = await createNewUser();
-//   //   expect(user).toBeTruthy();
+      const game = await getOrCreateDailyGame(user);
+      expect(game).toBeTruthy();
 
-//   //   const game = await createNewGame(user);
-//   //   expect(game).toBeTruthy();
+      const attempt = await createNewAttempt({
+        game,
+        attempt: 'attempt',
+        feedback: 'feedback',
+      });
+      expect(attempt).toBeTruthy();
 
-//   //   const attempt = await createNewAttempt({
-//   //     game,
-//   //     attempt: 'attempt',
-//   //     feedback: 'feedback',
-//   //   });
-//   //   expect(attempt).toBeTruthy();
-//   // });
+      const attempts = await getAttempts(game);
+      expect(attempts).toBeTruthy();
+      expect(attempts).toHaveLength(1);
+    });
+  });
 
-//   // FIXME: add test case
-//   test.skip('verify attempts are returned in order', async () => {});
-// });
+  test.describe('adhoc game', () => {
+    test('get attempts', async () => {
+      const user = await createNewUser();
+      expect(user).toBeTruthy();
+
+      const game = await createNewAdhocGame(user);
+      expect(game).toBeTruthy();
+
+      const attempts = await getAttempts(game);
+      expect(attempts).toBeTruthy();
+      expect(attempts).toHaveLength(0);
+    });
+
+    test('make attempt', async () => {
+      const user = await createNewUser();
+      expect(user).toBeTruthy();
+
+      const game = await createNewAdhocGame(user);
+      expect(game).toBeTruthy();
+
+      const attempt = await createNewAttempt({
+        game,
+        attempt: 'attempt',
+        feedback: 'feedback',
+      });
+      expect(attempt).toBeTruthy();
+
+      const attempts = await getAttempts(game);
+      expect(attempts).toBeTruthy();
+      expect(attempts).toHaveLength(1);
+    });
+  });
+
+  test('verify attempts are returned in the order they were created', async () => {
+    const user = await createNewUser();
+    expect(user).toBeTruthy();
+
+    const game = await createNewAdhocGame(user);
+    expect(game).toBeTruthy();
+
+    for (let i = 0; i < 5; i++) {
+      const attempt = await createNewAttempt({
+        game,
+        attempt: 'attempt',
+        feedback: 'feedback',
+      });
+      expect(attempt).toBeTruthy();
+    }
+
+    const attempts = await getAttempts(game);
+    expect(attempts).toBeTruthy();
+    expect(attempts).toHaveLength(5);
+
+    const first = attempts.shift()!.created_at.toISOString();
+    const last = attempts.pop()!.created_at.toISOString();
+    expect(first < last).toBe(true);
+  });
+});
--- src/db/adapters/attempts.ts
+++ src/db/adapters/attempts.ts
@@ -1,22 +1,36 @@
-import { asc, sql } from 'drizzle-orm';
+import { asc, eq } from 'drizzle-orm';
 
 import { server } from '@/api/server';
+import type { AdhocGame } from '@/db/schema/adhoc_games';
 import { Attempt } from '@/db/schema/attempts';
-import { DailyGame } from '@/db/schema/daily_games';
+import type { DailyGame } from '@/db/schema/daily_games';
 
+type Props<T extends DailyGame | AdhocGame> = {
+  game: T;
+  attempt: string;
+  feedback: string;
+};
+
+export async function createNewAttempt(
+  props: Props<DailyGame>,
+): Promise<Attempt>;
+export async function createNewAttempt(
+  props: Props<AdhocGame>,
+): Promise<Attempt>;
 export async function createNewAttempt({
   game,
   attempt: value,
   feedback,
-}: {
-  game: DailyGame;
-  attempt: string;
-  feedback: string;
-}): Promise<Attempt> {
+}: Props<DailyGame | AdhocGame>): Promise<Attempt> {
   server.log.info('create new attempt');
+  const isDailyGame = 'solution_id' in game;
   const attempts = await server.db
     .insert(Attempt)
-    .values({ game_id: game.id, value, feedback })
+    .values({
+      [isDailyGame ? 'daily_game_id' : 'adhoc_game_id']: game.id,
+      value,
+      feedback,
+    })
     .returning();
   const attempt = attempts.pop();
   if (!attempt) {
@@ -25,11 +39,21 @@ export async function createNewAttempt({
   return attempt;
 }
 
-export async function getAttempts(game: DailyGame): Promise<Attempt[]> {
+export function getAttempts(game: DailyGame): Promise<Attempt[]>;
+export function getAttempts(game: AdhocGame): Promise<Attempt[]>;
+export async function getAttempts(
+  game: DailyGame | AdhocGame,
+): Promise<Attempt[]> {
+  server.log.info('get attempts');
+  const isDailyGame = 'solution_id' in game;
   const attempts = await server.db
     .select()
     .from(Attempt)
-    .where(sql`${Attempt.game_id} = ${game.id}`)
+    .where(
+      isDailyGame
+        ? eq(Attempt.daily_game_id, game.id)
+        : eq(Attempt.adhoc_game_id, game.id),
+    )
     .orderBy(asc(Attempt.created_at));
   return attempts;
 }
