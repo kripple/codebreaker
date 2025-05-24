64364ef4a530fdd77c821a274b751d25c8620a99
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-05-24 13:12:26 -0400

    update tests

--- src/api/helpers/codemaker.test.ts
+++ src/api/helpers/codemaker.test.ts
@@ -2,25 +2,27 @@ import { expect, test } from '@playwright/test';
 
 import { evaluateAttempt } from '@/api/helpers/codemaker';
 
-const testCases = [
-  { solution: '1111', input: '1111', expected: 'XXXX' },
-  { solution: '1111', input: '2222', expected: '----' },
+test.describe('codemaker', () => {
+  const testCases = [
+    { solution: '1111', input: '1111', expected: 'XXXX' },
+    { solution: '1111', input: '2222', expected: '----' },
 
-  { solution: '1122', input: '1111', expected: 'XX--' },
-  { solution: '1122', input: '3333', expected: '----' },
-  { solution: '1122', input: '2211', expected: 'OOOO' },
-  { solution: '1122', input: '1122', expected: 'XXXX' },
-  { solution: '1122', input: '1212', expected: 'XXOO' },
+    { solution: '1122', input: '1111', expected: 'XX--' },
+    { solution: '1122', input: '3333', expected: '----' },
+    { solution: '1122', input: '2211', expected: 'OOOO' },
+    { solution: '1122', input: '1122', expected: 'XXXX' },
+    { solution: '1122', input: '1212', expected: 'XXOO' },
 
-  // '1112',
-  // '1122',
-  // '1123',
-  // '1234',
-];
+    // '1112',
+    // '1122',
+    // '1123',
+    // '1234',
+  ];
 
-testCases.map(({ solution, input, expected }) => {
-  test(`evaluates attempt '${input}' for solution '${solution}'`, () => {
-    const actual = evaluateAttempt(input, solution);
-    expect(actual.feedback).toEqual(expected);
+  testCases.map(({ solution, input, expected }) => {
+    test(`evaluates attempt '${input}' for solution '${solution}'`, () => {
+      const actual = evaluateAttempt(input, solution);
+      expect(actual.feedback).toEqual(expected);
+    });
   });
 });
