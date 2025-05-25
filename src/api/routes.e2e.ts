11d676ecf4fdef503e69d5ea01c24db0f1ec27bf
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-05-25 10:34:45 -0400

    refactor route tests

--- src/api/routes.e2e.ts
+++ src/api/routes.e2e.ts
@@ -1,55 +1,81 @@
-import { expect, test } from '@playwright/test';
+import { type APIRequestContext, expect, test } from '@playwright/test';
 
 const port = 3000 as const;
 const url = `http://localhost:${port}` as const;
 
+type Actual = ReturnType<typeof expect>;
+
 test.describe('routes', () => {
-  test('happy path', async ({ request }) => {
-    // /game/new
-    const response = await request.get(`${url}/game/new`);
+  async function expectResponse({
+    expectProps,
+    method,
+    request,
+    url,
+  }: {
+    expectProps: { [key: string]: (e: Actual) => void };
+    method: 'get' | 'post';
+    request: APIRequestContext;
+    url: string;
+  }) {
+    const response = await request[method](url);
     expect(response.status()).toBe(200);
     const body = await response.json();
-    expect(body).toHaveProperty('data');
-    const data = body.data;
-    expect(data).toHaveProperty('id');
-    expect(data).toHaveProperty('attempts');
-    const { id, attempts } = data;
-    expect(id).toBeTruthy();
-    expect(attempts).toHaveLength(0);
+    expect(body).toBeTruthy();
+    Object.entries(expectProps).map(([key, callbackFn]) => {
+      expect(body).toHaveProperty(key);
+      callbackFn(expect(body[key]));
+    });
+    return body;
+  }
+
+  test('happy path', async ({ request }) => {
+    // /game/new
+    const { id } = await expectResponse({
+      expectProps: {
+        id: (e: Actual) => e.toBeTruthy(),
+        attempts: (e: Actual) => e.toHaveLength(0),
+      },
+      method: 'get',
+      request,
+      url: `${url}/game/new`,
+    });
 
     // /game/:id
-    const responseById = await request.get(`${url}/game/${id}`);
-    expect(responseById.status()).toBe(200);
-    const bodyById = await response.json();
-    expect(bodyById).toHaveProperty('data');
-    const dataById = bodyById.data;
-    expect(dataById).toHaveProperty('id');
-    expect(dataById).toHaveProperty('attempts');
-    const { id: alsoId, attempts: alsoAttempts } = dataById;
-    expect(id).toEqual(alsoId);
-    expect(alsoAttempts).toHaveLength(0);
+    await expectResponse({
+      expectProps: {
+        id: (e: Actual) => e.toEqual(id),
+        attempts: (e: Actual) => e.toHaveLength(0),
+      },
+      method: 'get',
+      request,
+      url: `${url}/game/${id}`,
+    });
 
     // /game/:id/try/:code
-    const attemptsResponse = await request.post(`${url}/game/${id}/try/1234`);
-    expect(attemptsResponse.status()).toBe(200);
-    const attemptsBody = await attemptsResponse.json();
-    expect(attemptsBody).toHaveProperty('data');
-    const attemptsData = attemptsBody.data;
-    expect(attemptsData).toHaveProperty('id');
-    expect(attemptsData).not.toHaveProperty('attempts');
-    const { id: attemptsResponseId } = attemptsData;
-    expect(id).toEqual(attemptsResponseId);
+    await expectResponse({
+      expectProps: {
+        id: (e: Actual) => e.toEqual(id),
+      },
+      method: 'post',
+      request,
+      url: `${url}/game/${id}/try/1234`,
+    });
 
     // /game/:id (again)
-    const updatedResponse = await request.get(`${url}/game/${id}`);
-    expect(updatedResponse.status()).toBe(200);
-    const updatedBody = await updatedResponse.json();
-    expect(updatedBody).toHaveProperty('data');
-    const updatedData = updatedBody.data;
-    expect(updatedData).toHaveProperty('id');
-    expect(updatedData).toHaveProperty('attempts');
-    const { id: updatedId, attempts: updatedAttempts } = updatedData;
-    expect(id).toEqual(updatedId);
-    expect(updatedAttempts).toHaveLength(1);
+    await expectResponse({
+      expectProps: {
+        id: (e: Actual) => e.toEqual(id),
+        attempts: (e: Actual) => {
+          e.toHaveLength(1);
+          e.toContainEqual({
+            feedback: expect.any(String),
+            value: expect.any(String),
+          });
+        },
+      },
+      method: 'get',
+      request,
+      url: `${url}/game/${id}`,
+    });
   });
 });
