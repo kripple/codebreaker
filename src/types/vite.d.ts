77756369c386fc1b37ede689ec055c3360c9b81c
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-05-26 12:13:54 -0400

    add built-in vite env vars to type def

--- src/types/vite.d.ts
+++ src/types/vite.d.ts
@@ -1,4 +1,11 @@
 interface ImportMetaEnv {
+  // built-in env variables
+  readonly MODE: string;
+  readonly BASE_URL: string;
+  readonly PROD: string;
+  readonly DEV: string;
+  readonly SSR: string;
+  // everything else requires VITE_ prefix to prevent exposure of server vars in the browser
   readonly VITE_API_URL: string;
 }
 
