b8a56d88bfa28039665d54d533ae501c0c043ef3
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-05-28 13:41:41 -0400

    add msw

---
+++ src/app/mocks/handlers.ts
@@ -0,0 +1,39 @@
+import { HttpResponse, http } from 'msw';
+
+export const handlers = [
+  http.get(`${import.meta.env.VITE_API_URL}/game/:id`, ({ params }) => {
+    return HttpResponse.json({
+      id: params.id,
+      attempts: [
+        {
+          value: '1111',
+          feedback: '----',
+        },
+        {
+          value: '2222',
+          feedback: '----',
+        },
+        {
+          value: '3333',
+          feedback: 'XX--',
+        },
+        {
+          value: '3344',
+          feedback: 'XX--',
+        },
+        {
+          value: '3355',
+          feedback: 'XX--',
+        },
+        {
+          value: '3366',
+          feedback: 'XX--',
+        },
+        {
+          value: '3377',
+          feedback: 'XXXX',
+        },
+      ],
+    });
+  }),
+];
--- src/types/vite.d.ts
+++ src/types/vite.d.ts
@@ -1,10 +1,10 @@
 interface ImportMetaEnv {
   // built-in env variables
   readonly MODE: string;
-  // readonly BASE_URL: string;
-  // readonly PROD: string;
-  // readonly DEV: string;
-  // readonly SSR: string;
+  readonly BASE_URL: string;
+  readonly PROD: string;
+  readonly DEV: string;
+  readonly SSR: string;
   // everything else requires VITE_ prefix to prevent exposure of server vars in the browser
   readonly VITE_API_URL: string;
   readonly VITE_APP_URL: string;
