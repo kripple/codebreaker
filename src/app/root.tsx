c7b9eccc36324bd0ff178771b53ff8f308cc3329
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-05-29 13:13:26 -0400

    disable msw for now

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -1,4 +1,4 @@
-import type { StartOptions } from 'msw/browser';
+// import type { StartOptions } from 'msw/browser';
 import { createRoot } from 'react-dom/client';
 
 import { App } from '@/app/components/App';
@@ -8,17 +8,17 @@ import '@/app/root.css';
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 async function enableMocking() {
-  if (!import.meta.env.DEV) return;
+  // if (!import.meta.env.DEV) return;
 
-  const workerStartOptions: StartOptions = {
-    onUnhandledRequest: 'error',
-    serviceWorker: {
-      url: './mockServiceWorker.js',
-    },
-  };
+  // const workerStartOptions: StartOptions = {
+  //   onUnhandledRequest: 'error',
+  //   serviceWorker: {
+  //     url: './mockServiceWorker.js',
+  //   },
+  // };
 
-  const { worker } = await import('@/app/mocks/worker');
-  return worker.start(workerStartOptions);
+  // const { worker } = await import('@/app/mocks/worker');
+  // return worker.start(workerStartOptions);
 }
 
 enableMocking().then(() => {
