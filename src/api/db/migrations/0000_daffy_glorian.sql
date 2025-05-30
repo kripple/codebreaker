4e503efb8dcc8bcb916523b6a99e2408f95f15f8
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-05-30 16:57:39 -0400

    recreate base migration

--- src/api/db/migrations/0000_daffy_glorian.sql
+++ src/api/db/migrations/0000_daffy_glorian.sql
@@ -8,18 +8,16 @@ CREATE TABLE "adhoc_games" (
 );
 --> statement-breakpoint
 CREATE TABLE "attempts" (
-	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "attempts_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"game_id" integer NOT NULL,
+	"game_attempts_order" integer NOT NULL,
 	"value" varchar NOT NULL,
 	"feedback" varchar NOT NULL,
-	"daily_game_id" integer,
-	"adhoc_game_id" integer,
 	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
 	"updated_at" timestamp with time zone,
 	"deleted_at" timestamp with time zone,
-	CONSTRAINT "game_type" CHECK (
-      ("attempts"."daily_game_id" IS NOT NULL AND "attempts"."adhoc_game_id" IS NULL)
-      OR
-      ("attempts"."daily_game_id" IS NULL AND "attempts"."adhoc_game_id" IS NOT NULL)
+	CONSTRAINT "id" PRIMARY KEY("game_id","game_attempts_order"),
+	CONSTRAINT "game_attempts_order_check" CHECK (
+      ("attempts"."game_attempts_order" > 0 AND "attempts"."game_attempts_order" < 17)
     )
 );
 --> statement-breakpoint
@@ -30,6 +28,20 @@ CREATE TABLE "daily_games" (
 	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
 	"updated_at" timestamp with time zone,
 	"deleted_at" timestamp with time zone
+);
+--> statement-breakpoint
+CREATE TABLE "generic_games" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "generic_games_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"daily_game_id" integer,
+	"adhoc_game_id" integer,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone,
+	"deleted_at" timestamp with time zone,
+	CONSTRAINT "game_type" CHECK (
+      ("generic_games"."daily_game_id" IS NOT NULL AND "generic_games"."adhoc_game_id" IS NULL)
+      OR
+      ("generic_games"."daily_game_id" IS NULL AND "generic_games"."adhoc_game_id" IS NOT NULL)
+    )
 );
 --> statement-breakpoint
 CREATE TABLE "solutions" (
@@ -53,14 +65,16 @@ CREATE TABLE "users" (
 );
 --> statement-breakpoint
 ALTER TABLE "adhoc_games" ADD CONSTRAINT "adhoc_games_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "attempts" ADD CONSTRAINT "attempts_daily_game_id_daily_games_id_fk" FOREIGN KEY ("daily_game_id") REFERENCES "public"."daily_games"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "attempts" ADD CONSTRAINT "attempts_adhoc_game_id_adhoc_games_id_fk" FOREIGN KEY ("adhoc_game_id") REFERENCES "public"."adhoc_games"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "attempts" ADD CONSTRAINT "attempts_game_id_generic_games_id_fk" FOREIGN KEY ("game_id") REFERENCES "public"."generic_games"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "daily_games" ADD CONSTRAINT "daily_games_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "daily_games" ADD CONSTRAINT "daily_games_solution_id_solutions_id_fk" FOREIGN KEY ("solution_id") REFERENCES "public"."solutions"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "generic_games" ADD CONSTRAINT "generic_games_daily_game_id_daily_games_id_fk" FOREIGN KEY ("daily_game_id") REFERENCES "public"."daily_games"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "generic_games" ADD CONSTRAINT "generic_games_adhoc_game_id_adhoc_games_id_fk" FOREIGN KEY ("adhoc_game_id") REFERENCES "public"."adhoc_games"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 CREATE INDEX "adhoc_game_user_idx" ON "adhoc_games" USING btree ("user_id");--> statement-breakpoint
-CREATE INDEX "daily_game_attempt_idx" ON "attempts" USING btree ("daily_game_id");--> statement-breakpoint
-CREATE INDEX "adhoc_game_attempt_idx" ON "attempts" USING btree ("adhoc_game_id");--> statement-breakpoint
+CREATE INDEX "game_attempts_idx" ON "attempts" USING btree ("game_id");--> statement-breakpoint
 CREATE INDEX "daily_game_user_idx" ON "daily_games" USING btree ("user_id");--> statement-breakpoint
 CREATE INDEX "daily_game_solution_idx" ON "daily_games" USING btree ("solution_id");--> statement-breakpoint
+CREATE INDEX "daily_game_idx" ON "generic_games" USING btree ("daily_game_id");--> statement-breakpoint
+CREATE INDEX "adhoc_game_idx" ON "generic_games" USING btree ("adhoc_game_id");--> statement-breakpoint
 CREATE UNIQUE INDEX "daily_game_solution_date_idx" ON "solutions" USING btree ("date");--> statement-breakpoint
 CREATE UNIQUE INDEX "uuid_idx" ON "users" USING btree ("uuid");
