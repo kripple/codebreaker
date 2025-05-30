f3db56ed5e134b0609f53189c1284b3e24cc4ef5
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-05-30 16:32:45 -0400

    add range check and an index for attempts table

--- src/api/db/schema/attempts.ts
+++ src/api/db/schema/attempts.ts
@@ -1,6 +1,8 @@
+import { sql } from 'drizzle-orm';
 import {
+  check,
+  index,
   integer,
-  pgEnum,
   pgTable,
   primaryKey,
   varchar,
@@ -9,37 +11,30 @@ import {
 import { timestamps } from '@/api/db/helpers/timestamps';
 import { GenericGame } from '@/api/db/schema/generic_games';
 
-export const turnsEnum = pgEnum('turns', [
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '10',
-  '11',
-  '12',
-  '13',
-  '14',
-  '15',
-  '16',
-]);
-
 export const Attempt = pgTable(
   'attempts',
   {
     game_id: integer()
       .references(() => GenericGame.id)
       .notNull(),
-    turn: turnsEnum().notNull(),
+    game_attempts_order: integer().notNull(),
     value: varchar().notNull(),
     feedback: varchar().notNull(),
     ...timestamps,
   },
-  (table) => [primaryKey({ columns: [table.game_id, table.turn] })],
+  (table) => [
+    check(
+      'game_attempts_order_check',
+      sql`
+      (${table.game_attempts_order} > 0 AND ${table.game_attempts_order} < 17)
+    `,
+    ),
+    primaryKey({
+      name: 'id',
+      columns: [table.game_id, table.game_attempts_order],
+    }),
+    index('game_attempts_idx').on(table.game_id),
+  ],
 );
 
 export type Attempt = typeof Attempt.$inferSelect;
