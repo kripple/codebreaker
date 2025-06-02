9f9e269ba7d8e1b295cac1505abc6e1dccf2edbd
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-06-02 17:20:13 -0400

    try using lighthouse with puppeteer

--- src/utils/lighthouse.ts
+++ src/utils/lighthouse.ts
@@ -1,28 +1,23 @@
-import * as chromeLauncher from 'chrome-launcher';
-import lighthouse, { type Flags } from 'lighthouse';
+import lighthouse from 'lighthouse';
+import puppeteer from 'puppeteer';
 
 const failureThreshold = 0.9 as const;
 const url = 'https://kripple.github.io/codebreaker/' as const;
+const port = 9222 as const;
 
-// configure chrome for ci environments
-const chromeFlags = [
-  '--headless',
-  '--no-sandbox',
-  '--disable-gpu',
-  '--disable-dev-shm-usage',
-];
+const browser = await puppeteer.launch({
+  headless: true,
+  args: [`--remote-debugging-port=${port}`],
+});
+const result = await lighthouse(url, { port });
+if (!result) throw Error('failed to create lighthouse report');
 
-const chrome = await chromeLauncher.launch({ chromeFlags });
-const options: Flags = { port: chrome.port };
-const runnerResult = await lighthouse(url, options);
-if (!runnerResult) throw Error('missing runner result');
-
-for (const category in runnerResult.lhr.categories) {
-  const score = runnerResult.lhr.categories[category].score;
+for (const category in result.lhr.categories) {
+  const score = result.lhr.categories[category].score;
   if (!score) throw Error(`missing score for '${category}' category`);
   if (score < failureThreshold)
     console.warn(`failed ${category} audit with a score of ${score * 100}`);
   console.info(`${category}: ${score * 100}`);
 }
 
-chrome.kill();
+await browser.close();
