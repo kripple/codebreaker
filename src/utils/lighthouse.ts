02ab1f162682e4ec0bf7375e871dd0afe1581558
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-06-02 17:06:12 -0400

    better comments

--- src/utils/lighthouse.ts
+++ src/utils/lighthouse.ts
@@ -4,18 +4,16 @@ import lighthouse, { type Flags } from 'lighthouse';
 const failureThreshold = 0.9 as const;
 const url = 'https://kripple.github.io/codebreaker/' as const;
 
-const chrome = await chromeLauncher.launch({
-  // This combination fixes many common Chrome launch failures in CI environments like GitHub Actions.
-  chromeFlags: [
-    '--headless',
-    '--no-sandbox',
-    '--disable-gpu',
-    '--disable-dev-shm-usage',
-  ],
-});
-const options: Flags = {
-  port: chrome.port,
-};
+// configure chrome for ci environments
+const chromeFlags = [
+  '--headless',
+  '--no-sandbox',
+  '--disable-gpu',
+  '--disable-dev-shm-usage',
+];
+
+const chrome = await chromeLauncher.launch({ chromeFlags });
+const options: Flags = { port: chrome.port };
 const runnerResult = await lighthouse(url, options);
 if (!runnerResult) throw Error('missing runner result');
 
