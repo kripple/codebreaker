68f716eee6699cd9409359ad16d6cae51de30ca4
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-06-02 21:13:01 -0400

    refactor lighthouse script

--- src/utils/lighthouse.ts
+++ src/utils/lighthouse.ts
@@ -1,23 +1,34 @@
 import lighthouse from 'lighthouse';
 import puppeteer from 'puppeteer';
 
-const failureThreshold = 0.9 as const;
+const failureThreshold = 90 as const;
 const url = 'https://kripple.github.io/codebreaker/' as const;
 const port = 9222 as const;
-
 const browser = await puppeteer.launch({
   headless: true,
-  args: ['--no-sandbox', `--remote-debugging-port=${port}`],
+  args: [
+    '--disable-dev-shm-usage',
+    '--disable-extensions',
+    '--disable-gpu',
+    '--disable-infobars',
+    '--disable-setuid-sandbox',
+    '--no-sandbox',
+    '--no-zygote',
+    `--remote-debugging-port=${port}`,
+  ],
 });
 const result = await lighthouse(url, { port });
 if (!result) throw Error('failed to create lighthouse report');
 
 for (const category in result.lhr.categories) {
-  const score = result.lhr.categories[category].score;
-  if (!score) throw Error(`missing score for '${category}' category`);
+  const draft = result.lhr.categories[category].score;
+  if (!draft) throw Error(`missing score for '${category}' category`);
+
+  const score = draft * 100;
   if (score < failureThreshold)
-    throw Error(`failed ${category} audit with a score of ${score * 100}`);
-  console.info(`${category}: ${score * 100}`);
+    throw Error(`failed ${category} audit with a score of ${score}`);
+
+  console.info(`${category}: ${score}`);
 }
 
 await browser.close();
